1ccd9196015eaa8c9c2d8ccbf5d508f98097a853
Estefania Calderon <ecalderon@endava.com>
2018-01-16 15:40:10 -0500

    clase Ejercicio

---
+++ BasicGitInternship/src/main/java/Impl/users/EstefaniaCalderon.java
@@ -0,0 +1,20 @@
+package Impl.users;
+
+public class EstefaniaCalderon {
+
+
+    String id = "1026567350";
+    String name = "Estefania";
+    String lastName = "Calderon Martinez";
+
+    public EstefaniaCalderon() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFullName(){
+        return name +" "+ lastName;
+    }
+}
