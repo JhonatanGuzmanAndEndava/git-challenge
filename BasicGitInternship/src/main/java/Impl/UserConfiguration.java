56d5a137225ab89d4717c0fbb9c246b52be408ef
Miguel Ballen <mballen@endava.com>
2018-01-16 16:50:23 -0500

    Second commit

--- BasicGitInternship/src/main/java/Impl/UserConfiguration.java
+++ BasicGitInternship/src/main/java/Impl/UserConfiguration.java
@@ -15,6 +15,7 @@ public class UserConfiguration {
         users.put("1098326512" , "Raúl Fandiño");
         users.put("451191" , "Alejandra Chacón");
         users.put("1014282537" , "Luis Zarate");
+        users.put("1031135661" , "Miguel Ballen");
 		//Add the new users here ...
 
 
