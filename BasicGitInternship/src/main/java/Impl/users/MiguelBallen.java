a24c9b8f30b27b7a29e2cfcfa62a203e96bae3e5
Miguel Ballen <mballen@endava.com>
2018-01-16 16:43:38 -0500

    First commit

---
+++ BasicGitInternship/src/main/java/Impl/users/MiguelBallen.java
@@ -0,0 +1,21 @@
+package Impl.users;
+
+/**
+ * Created by rfandino on 1/15/2018.
+ */
+public class MiguelBallen{
+    string id = "1031135661";
+    string name = "Miguel Angel";
+    string lastname = "Ballen Galindo";
+
+    public MiguelBallen() {
+    }
+
+    public string getid() {
+        return id;
+    }
+
+    public string getfullname(){
+        return name +" "+ lastname;
+    }
+}
