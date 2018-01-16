3ad56980d973fb3b9c2c2066001a65a6913fe075
Sebastian Puerto <spuerto@endava.com>
2018-01-16 16:59:53 -0500

    Sebastian

--- BasicGitInternship/src/main/java/Impl/UserConfiguration.java
+++ BasicGitInternship/src/main/java/Impl/UserConfiguration.java
@@ -15,8 +15,8 @@ public class UserConfiguration {
         users.put("1098326512" , "Raúl Fandiño");
         users.put("451191" , "Alejandra Chacón");
         users.put("1014282537" , "Luis Zarate");
+        users.put("1032466534" , "Sebastian Puerto");
 		//Add the new users here ...
-
 
         return users;
     }
