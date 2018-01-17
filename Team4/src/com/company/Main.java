c4e4d1a3b67937e48a987b555adbdbb5e1cec265
Luis Zarate <lzarate@endava.com>
2018-01-17 13:35:08 -0500

    basic book info

--- Team4/src/com/company/Main.java
+++ Team4/src/com/company/Main.java
@@ -8,9 +8,27 @@ public class Main {
         System.out.println("1: Create book");
         Scanner sc = new Scanner(System.in);
         int option = sc.nextInt();
+        String name;
+        String author;
+        String ISBN;
+        String published;
+        String language;
+        String publisher;
         switch (option){
             case 1:
-                System.out.print("good");
+                System.out.print("Enter book´s name: ");
+                name = sc.next();
+                System.out.print("Enter book´s author: ");
+                author = sc.next();
+                System.out.print("Enter book´s ISBN code: ");
+                ISBN = sc.next();
+                System.out.print("Enter published date: ");
+                published = sc.next();
+                System.out.print("Enter book´s language: ");
+                language = sc.next();
+                System.out.print("Enter book´s publisher: ");
+                publisher = sc.next();
+                Libro lib = new Libro(name,author,ISBN,published,language,publisher);
                 break;
         }
 
