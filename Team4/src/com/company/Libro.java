6c42e7071d301a632a5e86307e683dcef80914b5
Sebastian Puerto <spuerto@endava.com>
2018-01-17 11:46:06 -0500

    Gett's and Set's

--- Team4/src/com/company/Libro.java
+++ Team4/src/com/company/Libro.java
@@ -16,4 +16,42 @@ public class Libro {
         this.language = language;
         this.publisher = publisher;
     }
+
+    //Getters and Setters
+    public String getname(){
+        return name;
+    }
+    public void setname(String name){
+        this.name = name;
+    }
+    public String getauthor(){
+        return author;
+    }
+    public void setauthor(String author){
+        this.author = author;
+    }
+    public String getISBN(){
+        return ISBN;
+    }
+    public void setISBN(String ISBN){
+        this.ISBN = ISBN;
+    }
+    public String getpublished(){
+        return published;
+    }
+    public void setpublished(String published){
+        this.published = published;
+    }
+    public String getlenguage(){
+        return lenguage;
+    }
+    public void setlenguage(String lenguage){
+        this.lenguage = lenguage;
+    }
+    public String getpublisher(){
+        return publisher;
+    }
+    public void setpublisher(String publisher){
+        this.publisher = publisher;
+    }
 }
