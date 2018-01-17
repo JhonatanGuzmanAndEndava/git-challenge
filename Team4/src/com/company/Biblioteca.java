80d55e78ea95153f5b71cfafb0c48a1bed20dbfb
Luis Zarate <lzarate@endava.com>
2018-01-17 16:03:50 -0500

    Create ready

--- Team4/src/com/company/Biblioteca.java
+++ Team4/src/com/company/Biblioteca.java
@@ -13,13 +13,33 @@ public class Biblioteca {
                 FileWriter fileWriter = new FileWriter(data);
                 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
                 PrintWriter printWriter = new PrintWriter(bufferedWriter);
-                printWriter.write(lib.getISBN());
+                printWriter.write("ISBN code: " + lib.getISBN() + "\n");
+                printWriter.write("Name: " + lib.getname() + "\n");
+                printWriter.write("Author: " + lib.getauthor() + "\n");
+                printWriter.write("Published: " + lib.getpublished() + "\n");
+                printWriter.write("Language: " + lib.getlenguage() + "\n");
+                printWriter.write("Publisher: " + lib.getpublisher() + "\n");
                 printWriter.close();
                 bufferedWriter.close();
                 fileWriter.close();
             }else{
-                System.out.println("ya existo");
+                System.out.println("existo");
+                FileWriter fileWriter = new FileWriter(data, true);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                PrintWriter printWriter = new PrintWriter(bufferedWriter);
+                printWriter.write("\n");
+                printWriter.write("ISBN code: " + lib.getISBN() + "\n");
+                printWriter.write("Name: " + lib.getname() + "\n");
+                printWriter.write("Author: " + lib.getauthor() + "\n");
+                printWriter.write("Published: " + lib.getpublished() + "\n");
+                printWriter.write("Language: " + lib.getlenguage() + "\n");
+                printWriter.write("Publisher: " + lib.getpublisher() + "\n");
+                printWriter.close();
+                bufferedWriter.close();
+                fileWriter.close();
+
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
--- Team4/src/com/company/Main.java
+++ Team4/src/com/company/Main.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("1: Create book");
-        Scanner sc = new Scanner(System.in);
-        int option = sc.nextInt();
+        boolean exit = false;
+        int option;
         Biblioteca library = new Biblioteca();
         String name;
         String author;
@@ -15,24 +14,36 @@ public class Main {
         String published;
         String language;
         String publisher;
-        switch (option){
-            case 1:
-                System.out.print("Enter book´s name: ");
-                name = sc.next();
-                System.out.print("Enter book´s author: ");
-                author = sc.next();
-                System.out.print("Enter book´s ISBN code: ");
-                ISBN = sc.next();
-                System.out.print("Enter published date: ");
-                published = sc.next();
-                System.out.print("Enter book´s language: ");
-                language = sc.next();
-                System.out.print("Enter book´s publisher: ");
-                publisher = sc.next();
-                Libro lib = new Libro(name,author,ISBN,published,language,publisher);
-                library.createBook(lib);
-                break;
+        while(exit==false){
+            System.out.println("Please choose one option");
+            System.out.println("1: Create book");
+            System.out.println("0: Exit");
+            Scanner sc = new Scanner(System.in);
+            option = sc.nextInt();
+            sc.nextLine();
+            switch (option){
+                case 1:
+                    System.out.print("Enter book´s name: ");
+                    name = sc.nextLine();
+                    System.out.print("Enter book´s author: ");
+                    author = sc.nextLine();
+                    System.out.print("Enter book´s ISBN code: ");
+                    ISBN = sc.nextLine();
+                    System.out.print("Enter published date: ");
+                    published = sc.nextLine();
+                    System.out.print("Enter book´s language: ");
+                    language = sc.nextLine();
+                    System.out.print("Enter book´s publisher: ");
+                    publisher = sc.nextLine();
+                    Libro lib = new Libro(name,author,ISBN,published,language,publisher);
+                    library.createBook(lib);
+                    break;
+                case 0:
+                    exit = true;
+                    break;
+            }
         }
+
 
     }
 }
